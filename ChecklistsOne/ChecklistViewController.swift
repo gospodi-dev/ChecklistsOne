945d9139a8c0a3462cb37ba70b02b333abacfe05
Gospodi <gospodijazzet@gmail.com>
2022-02-23 18:25:34 +0300

    Прощаем код отображения флажков в ячейках

--- ChecklistsOne/ChecklistViewController.swift
+++ ChecklistsOne/ChecklistViewController.swift
@@ -65,32 +65,22 @@ class ChecklistViewController: UITableViewController {
       didSelectRowAt indexPath: IndexPath
     ) {
       if let cell = tableView.cellForRow(at: indexPath) {
-        var isChecked = false
-
         if indexPath.row == 0 {
-           row0checked.toggle()
-           isChecked = row0checked
-         } else if indexPath.row == 1 {
-           row1checked.toggle()
-           isChecked = row1checked
-         } else if indexPath.row == 2 {
-           row2checked.toggle()
-           isChecked = row2checked
-         } else if indexPath.row == 3 {
-           row3checked.toggle()
-           isChecked = row3checked
-         } else if indexPath.row == 4 {
-           row4checked.toggle()
-           isChecked = row4checked
-         }
-         if isChecked {
-           cell.accessoryType = .checkmark
-         } else {
-           cell.accessoryType = .none
-         }
-       }
+          row0checked.toggle()
+        } else if indexPath.row == 1 {
+          row1checked.toggle()
+        } else if indexPath.row == 2 {
+          row2checked.toggle()
+        } else if indexPath.row == 3 {
+          row3checked.toggle()
+        } else if indexPath.row == 4 {
+          row4checked.toggle()
+        }
+        configureCheckmark(for: cell, at: indexPath)
+      }
       tableView.deselectRow(at: indexPath, animated: true)
     }
+
     
     func configureCheckmark(
       for cell: UITableViewCell,
