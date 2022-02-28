7edbaccc3ce0d30e0f45d12e18df9bcbe8f7fe61
Gospodi <gospodijazzet@gmail.com>
2022-02-28 21:37:37 +0300

    Добавлен новый контроллер для страницы добавления строк

---
+++ ChecklistsOne/AddItemViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddItemViewController.swift
+//  ChecklistsOne
+//
+//  Created by Gospodi on 28.02.2022.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
