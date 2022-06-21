62719e1c54a91ec891f1ff20d4adab32d63cf649
Gospodi <gospodijazzet@gmail.com>
2022-06-22 01:57:31 +0300

    подключен делегат текстового поля

--- ChecklistsOne/AddItemViewController.swift
+++ ChecklistsOne/AddItemViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var textField: UITextField!
