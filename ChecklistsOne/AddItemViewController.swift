e3931d895ae76e4923e6304ccddec8635d6fe559
Gospodi <gospodijazzet@gmail.com>
2022-06-22 13:07:16 +0300

    добавлено оповещение делегата при нажатии кнопок отмена и готово

--- ChecklistsOne/AddItemViewController.swift
+++ ChecklistsOne/AddItemViewController.swift
@@ -22,6 +22,8 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
+    weak var delegate: AddItemViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,12 +34,14 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
     // MARK: - Actions
     // добавляем действия при нажатии на кнопки
     @IBAction func cancel() {
-        navigationController?.popViewController(animated: true)
+      delegate?.addItemViewControllerDidCancel(self)
     }
-    
+
     @IBAction func done() {
-        print("Contents of the text field: \(textField.text!)")
-        navigationController?.popViewController(animated: true)
+      let item = ChecklistItem()
+      item.text = textField.text!
+
+      delegate?.addItemViewController(self, didFinishAdding: item)
     }
     
     // MARK: - Table View Delegates
