dbf5441b6f2c49e51971199d304c1c9ecdd9e056
Gospodi <gospodijazzet@gmail.com>
2022-06-23 16:43:44 +0300

    добавлен новый шеврон

--- ChecklistsOne/ChecklistViewController.swift
+++ ChecklistsOne/ChecklistViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
     // MARK: - Add Item ViewController Delegates
     func addItemViewControllerDidCancel(
-      _ controller: AddItemViewController
+        _ controller: AddItemViewController
     ) {
-      navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
-
+    
     func addItemViewController(
-      _ controller: AddItemViewController,
-      didFinishAdding item: ChecklistItem
+        _ controller: AddItemViewController,
+        didFinishAdding item: ChecklistItem
     ) {
-      let newRowIndex = items.count
-      items.append(item)
-
-      let indexPath = IndexPath(row: newRowIndex, section: 0)
-      let indexPaths = [indexPath]
-      tableView.insertRows(at: indexPaths, with: .automatic)
-      navigationController?.popViewController(animated:true)
+        let newRowIndex = items.count
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        navigationController?.popViewController(animated:true)
     }
     
     var items = [ChecklistItem]()
@@ -62,16 +62,16 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     
     // MARK: - Navigation
     override func prepare(
-      for segue: UIStoryboardSegue,
-      sender: Any?
+        for segue: UIStoryboardSegue,
+        sender: Any?
     ) {
-      // 1
-      if segue.identifier == "AddItem" {
-        // 2
-        let controller = segue.destination as! AddItemViewController
-        // 3
-        controller.delegate = self
-      }
+        // 1
+        if segue.identifier == "AddItem" {
+            // 2
+            let controller = segue.destination as! AddItemViewController
+            // 3
+            controller.delegate = self
+        }
     }
     
     // MARK: - Table View Data Source
@@ -83,66 +83,77 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     }
     
     override func tableView(
-      _ tableView: UITableView,
-      cellForRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(
-        withIdentifier: "ChecklistItem",
-        for: indexPath)
-
-      let item = items[indexPath.row]
-
-      configureText(for: cell, with: item)
-      configureCheckmark(for: cell, with: item)
-      return cell
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "ChecklistItem",
+            for: indexPath)
+        
+        let item = items[indexPath.row]
+        
+        configureText(for: cell, with: item)
+        func configureCheckmark(
+            for cell: UITableViewCell,
+            with item: ChecklistItem
+        ) {
+            let label = cell.viewWithTag(1001) as! UILabel
+            
+            if item.checked {
+                label.text = "√"
+            } else {
+                label.text = ""
+            }
+        }
+        return cell
     }
-
+    
     // MARK: - Table View Delegate
     override func tableView(
-      _ tableView: UITableView,
-      didSelectRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
     ) {
-      if let cell = tableView.cellForRow(at: indexPath) {
-        let item = items[indexPath.row]
-        item.checked.toggle()
-        configureCheckmark(for: cell, with: item)
-      }
-      tableView.deselectRow(at: indexPath, animated: true)
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let item = items[indexPath.row]
+            item.checked.toggle()
+            configureCheckmark(for: cell, with: item)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
     func configureCheckmark(
-      for cell: UITableViewCell,
-      with item: ChecklistItem
+        for cell: UITableViewCell,
+        with item: ChecklistItem
     ) {
-      if item.checked {
-        cell.accessoryType = .checkmark
-      } else {
-        cell.accessoryType = .none
-      }
+        if item.checked {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
-
+    
     func configureText(
-      for cell: UITableViewCell,
-      with item: ChecklistItem
+        for cell: UITableViewCell,
+        with item: ChecklistItem
     ) {
-      let label = cell.viewWithTag(1000) as! UILabel
-      label.text = item.text
+        let label = cell.viewWithTag(1000) as! UILabel
+        label.text = item.text
     }
     // добавим удаление строки методом Swipe-to-delete
     override func tableView(
-      _ tableView: UITableView,
-      commit editingStyle: UITableViewCell.EditingStyle,
-      forRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
     ) {
-      
-      items.remove(at: indexPath.row)
-
-      
-      let indexPaths = [indexPath]
-      tableView.deleteRows(at: indexPaths, with: .automatic)
+        
+        items.remove(at: indexPath.row)
+        
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
     }
-
+    
     //MARK: - Actions
-
+    
 }
 
