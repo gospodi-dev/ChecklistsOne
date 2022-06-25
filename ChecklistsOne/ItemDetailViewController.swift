9c4d61af9002e9dfbf1013d2ea502321913d4add
Gospodi <gospodijazzet@gmail.com>
2022-06-25 20:20:54 +0300

    fix code

--- ChecklistsOne/ItemDetailViewController.swift
+++ ChecklistsOne/ItemDetailViewController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 protocol ItemDetailViewControllerDelegate: AnyObject {
-    func itemDetailViewControllerDidCancel(
-        _ controller: ItemDetailViewController)
-    func itemDetailViewController(
-        _ controller: ItemDetailViewController,
-        didFinishAdding item: ChecklistItem
-    )
-    func itemDetailViewController(
-        _ controller: ItemDetailViewController,
-        didFinishEditing item: ChecklistItem
-    )
+  func itemDetailViewControllerDidCancel(
+    _ controller: ItemDetailViewController)
+  func itemDetailViewController(
+    _ controller: ItemDetailViewController,
+    didFinishAdding item: ChecklistItem
+  )
+  func itemDetailViewController(
+    _ controller: ItemDetailViewController,
+    didFinishEditing item: ChecklistItem
+  )
 }
 
 class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
@@ -41,58 +41,54 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     super.viewWillAppear(animated)
     textField.becomeFirstResponder()
   }
-    
-    // MARK: - Actions
-    // добавляем действия при нажатии на кнопки
-    @IBAction func cancel() {
-        delegate?.itemDetailViewControllerDidCancel(self)
-    }
-    
-    @IBAction func done() {
-        if let item = itemToEdit {
-            item.text = textField.text!
-            delegate?.itemDetailViewController(
-                self,
-                didFinishEditing: item)
-        } else {
-            let item = ChecklistItem()
-            item.text = textField.text!
-            delegate?.itemDetailViewController(self, didFinishAdding: item)
-        }
-    }
-    
-    // MARK: - Table View Delegates
-    // убираем выделение строки
-    override func tableView(
-        _ tableView: UITableView,
-        willSelectRowAt indexPath: IndexPath
-    ) -> IndexPath? {
-        return nil
-    }
 
-    
-    // MARK: - Text Field Delegates
-    // один из методов `UITextField` делегирования. Он вызывается каждый раз, когда пользователь изменяет текст, будь то нажатием на клавиатуру или с помощью cut/paste.
-    func textField(
-      _ textField: UITextField,
-      shouldChangeCharactersIn range: NSRange,
-      replacementString string: String
-    ) -> Bool {
-      let oldText = textField.text!
-      let stringRange = Range(range, in: oldText)!
-      let newText = oldText.replacingCharacters(
-        in: stringRange,
-        with: string)
-      if newText.isEmpty {
-        doneBarButton.isEnabled = false
-      } else {
-        doneBarButton.isEnabled = true
-      }
-      return true
-    }
+  // MARK: - Actions
+  @IBAction func cancel() {
+    delegate?.itemDetailViewControllerDidCancel(self)
+  }
 
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+  @IBAction func done() {
+    if let item = itemToEdit {
+      item.text = textField.text!
+      delegate?.itemDetailViewController(
+        self,
+        didFinishEditing: item)
+    } else {
+      let item = ChecklistItem()
+      item.text = textField.text!
+      delegate?.itemDetailViewController(self, didFinishAdding: item)
+    }
+  }
+
+  // MARK: - Table View Delegates
+  override func tableView(
+    _ tableView: UITableView,
+    willSelectRowAt indexPath: IndexPath
+  ) -> IndexPath? {
+    return nil
+  }
+
+  // MARK: - Text Field Delegates
+  func textField(
+    _ textField: UITextField,
+    shouldChangeCharactersIn range: NSRange,
+    replacementString string: String
+  ) -> Bool {
+    let oldText = textField.text!
+    let stringRange = Range(range, in: oldText)!
+    let newText = oldText.replacingCharacters(
+      in: stringRange,
+      with: string)
+    if newText.isEmpty {
       doneBarButton.isEnabled = false
-      return true
+    } else {
+      doneBarButton.isEnabled = true
     }
+    return true
+  }
+
+  func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    doneBarButton.isEnabled = false
+    return true
+  }
 }
