a9975701b138065d7b04f9feca6edecdd22aeac6
Gospodi <gospodijazzet@gmail.com>
2022-07-02 19:45:44 +0300

    возвращаем список объектов в новом массиве

--- ChecklistsOne/AllListsViewController.swift
+++ ChecklistsOne/AllListsViewController.swift
@@ -11,55 +11,49 @@ class AllListsViewController: UITableViewController {
     
     let cellIdentifier = "ChecklistCell"
     var lists = [Checklist]() // равносильно Array<Checklist>()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Включить большие заголовки
         navigationController?.navigationBar.prefersLargeTitles = true
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
         
-        // 1
-        var list = Checklist()
-        list.name = "Birthdays"
+        var list = Checklist(name: "Birthdays")
         lists.append(list)
-
-        // 2
-        list = Checklist()
-        list.name = "Groceries"
+        
+        list = Checklist(name: "Groceries")
         lists.append(list)
-
-        list = Checklist()
-        list.name = "Cool Apps"
+        
+        list = Checklist(name: "Cool Apps")
         lists.append(list)
-
-        list = Checklist()
-        list.name = "To Do"
+        
+        list = Checklist(name: "To Do")
         lists.append(list)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(
       _ tableView: UITableView,
       numberOfRowsInSection section: Int
     ) -> Int {
-      return 3
-    }
-
-    override func tableView(
-      _ tableView: UITableView,
-      cellForRowAt indexPath: IndexPath
-    ) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(
-        withIdentifier: cellIdentifier, for: indexPath)
-      cell.textLabel!.text = "List \(indexPath.row)"
-      return cell
+      return lists.count
     }
     
     override func tableView(
-      _ tableView: UITableView,
-      didSelectRowAt indexPath: IndexPath
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel!.text = "List \(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
     ) {
-      performSegue(withIdentifier: "ShowChecklist", sender: nil)
+        performSegue(withIdentifier: "ShowChecklist", sender: nil)
     }
 }
