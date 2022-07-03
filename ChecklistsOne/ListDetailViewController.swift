56a3d2dcd9d1cd985e81056d5aec5713d20d2bcb
Gospodi <gospodijazzet@gmail.com>
2022-07-03 22:24:38 +0300

    добавлен новый контроллер ListDetailViewController

---
+++ ChecklistsOne/ListDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ListDetailViewController.swift
+//  ChecklistsOne
+//
+//  Created by Gospodi on 03.07.2022.
+//
+
+import UIKit
+
+protocol ListDetailViewControllerDelegate: AnyObject {
+  func listDetailViewControllerDidCancel(
+    _ controller: ListDetailViewController)
+
+  func listDetailViewController(
+    _ controller: ListDetailViewController,
+    didFinishAdding checklist: Checklist
+  )
+
+  func listDetailViewController(
+    _ controller: ListDetailViewController,
+    didFinishEditing checklist: Checklist
+  )
+}
+
+class ListDetailViewController: UITableViewController, UITextFieldDelegate {
+  @IBOutlet var textField: UITextField!
+  @IBOutlet var doneBarButton: UIBarButtonItem!
+
+  weak var delegate: ListDetailViewControllerDelegate?
+
+  var checklistToEdit: Checklist?
+}
