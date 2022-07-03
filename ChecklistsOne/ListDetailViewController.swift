0b7f330d0345086db8cc3cd5ee8f6e857e149c04
Gospodi <gospodijazzet@gmail.com>
2022-07-03 22:26:39 +0300

    функция изменяет заголовок экрана, если пользователь редактирует существующий контрольный список, и помещает его имя в текстовое поле.

--- ChecklistsOne/ListDetailViewController.swift
+++ ChecklistsOne/ListDetailViewController.swift
@@ -8,25 +8,35 @@
 import UIKit
 
 protocol ListDetailViewControllerDelegate: AnyObject {
-  func listDetailViewControllerDidCancel(
-    _ controller: ListDetailViewController)
-
-  func listDetailViewController(
-    _ controller: ListDetailViewController,
-    didFinishAdding checklist: Checklist
-  )
-
-  func listDetailViewController(
-    _ controller: ListDetailViewController,
-    didFinishEditing checklist: Checklist
-  )
+    func listDetailViewControllerDidCancel(
+        _ controller: ListDetailViewController)
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishAdding checklist: Checklist
+    )
+    
+    func listDetailViewController(
+        _ controller: ListDetailViewController,
+        didFinishEditing checklist: Checklist
+    )
 }
 
 class ListDetailViewController: UITableViewController, UITextFieldDelegate {
-  @IBOutlet var textField: UITextField!
-  @IBOutlet var doneBarButton: UIBarButtonItem!
-
-  weak var delegate: ListDetailViewControllerDelegate?
-
-  var checklistToEdit: Checklist?
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
+    weak var delegate: ListDetailViewControllerDelegate?
+    
+    var checklistToEdit: Checklist?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let checklist = checklistToEdit {
+            title = "Edit Checklist"
+            textField.text = checklist.name
+            doneBarButton.isEnabled = true
+        }
+    }
 }
