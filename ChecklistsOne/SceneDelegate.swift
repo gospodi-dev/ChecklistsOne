a2f0df6dc38556dbed90883cb0b0701708a2dca0
Gospodi <gospodijazzet@gmail.com>
2022-07-07 19:01:28 +0300

    создаем `DataModel` объект и помещаем его в константу с именем `dataModel`

--- ChecklistsOne/SceneDelegate.swift
+++ ChecklistsOne/SceneDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    // Это создает `DataModel` объект и помещает его в константу с именем `dataModel`
+    let dataModel = DataModel()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
