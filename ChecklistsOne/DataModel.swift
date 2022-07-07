29610bea96f8bb0b762628e8dea338cc0869c1c1
Gospodi <gospodijazzet@gmail.com>
2022-07-07 21:44:36 +0300

    проверка данных при первом запуске

--- ChecklistsOne/DataModel.swift
+++ ChecklistsOne/DataModel.swift
@@ -11,20 +11,21 @@ class DataModel {
     var lists = [Checklist]()
     
     var indexOfSelectedChecklist: Int {
-      get {
-        return UserDefaults.standard.integer(
-          forKey: "ChecklistIndex")
-      }
-      set {
-        UserDefaults.standard.set(
-          newValue,
-          forKey: "ChecklistIndex")
-      }
+        get {
+            return UserDefaults.standard.integer(
+                forKey: "ChecklistIndex")
+        }
+        set {
+            UserDefaults.standard.set(
+                newValue,
+                forKey: "ChecklistIndex")
+        }
     }
     
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     // MARK: - Data Saving
@@ -67,8 +68,24 @@ class DataModel {
     
     // MARK: - Defaults
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1 ]
+        let dictionary = [
+            "ChecklistIndex": -1,
+            "FirstTime": true
+        ] as [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime() {
+      let userDefaults = UserDefaults.standard
+      let firstTime = userDefaults.bool(forKey: "FirstTime")
+
+      if firstTime {
+        let checklist = Checklist(name: "List")
+        lists.append(checklist)
+
+        indexOfSelectedChecklist = 0
+        userDefaults.set(false, forKey: "FirstTime")
+      }
     }
 }
 
