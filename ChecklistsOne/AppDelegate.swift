c08e18bd38f1abf3ba5a2055807cd813c4af29c6
Gospodi <gospodijazzet@gmail.com>
2022-07-09 17:58:52 +0300

    подключаем платформу пользовательских уведомлений

--- ChecklistsOne/AppDelegate.swift
+++ ChecklistsOne/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications // говорит компилятору, что мы собираемся использовать платформу пользовательских уведомлений
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
