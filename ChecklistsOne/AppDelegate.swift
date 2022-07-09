ca2c2b95f184f23d1ed3351f818ca63f741d8621
Gospodi <gospodijazzet@gmail.com>
2022-07-09 18:14:56 +0300

    рефакторинг

--- ChecklistsOne/AppDelegate.swift
+++ ChecklistsOne/AppDelegate.swift
@@ -15,27 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Notification authorization. Сообщаем iOS, что приложение хочет отправлять уведомления типа “alert” со звуковым эффектом
+        // Notification authorization
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) {granted, error in
-            if granted {
-                print("We have permission")
-            } else {
-                print("Permission denied")
-            }
-        }
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "I am a local notification"
-        content.sound = UNNotificationSound.default
-
-        let trigger = UNTimeIntervalNotificationTrigger(
-          timeInterval: 10,
-          repeats: false)
-        let request = UNNotificationRequest(
-          identifier: "MyNotification",
-          content: content,
-          trigger: trigger)
-        center.add(request)
         center.delegate = self
         return true
     }
